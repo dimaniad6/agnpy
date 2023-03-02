aeb26ab8cb24bcc39b693b8ab195dadfd609ab3b
Ilaria Viale <ilaria.viale.1@studenti.unipd.it>
2023-03-02 17:09:42 +0100

    Add new sync and psync data from M. Cerruti

--- agnpy/tesi/diagrams/results_proton.py
+++ agnpy/tesi/diagrams/results_proton.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 from astropy.constants import m_p, h, m_e,m_p
 from astropy.coordinates import Distance
 
-#load_mpl_rc()  # adopt agnpy plotting style
-plt.style.use('proton_synchrotron')
+load_mpl_rc()  # adopt agnpy plotting style
+#plt.style.use('proton_synchrotron')
 
 # Define source parameters PKS2155
 B = 80 * u.G
